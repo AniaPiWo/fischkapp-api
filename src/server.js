43212fe419e3c36aa37c668531f58098643ee044
AniaPiWo <snowflake@wp.pl>
2023-07-28 14:50:24 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -1,38 +1,38 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import morgan from "morgan";
 import { api } from "./api.js";
-import { config } from './modules/config.js';
+import { config } from "./modules/config.js";
 import { Fishka } from "./modules/model.js";
 
 const app = express();
-app.use(morgan("dev"))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 dotenv.config();
 
-app.use("/", api)
+app.use("/", api);
 
-app.use('/', (req, res) => {
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', '0');
-  res.send('Hello, World!');
+app.use("/", (req, res) => {
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  res.send("Hello, World!");
 });
 
-const port = 3000;
+const port = 4000;
 app.listen(port, async () => {
   console.log(colors.magenta(`Server is live, running on port: ${port}`));
 
   try {
     console.log(colors.green(`Connecting to MongoDB...`));
     await mongoose.connect(config.MONGODB_URI, {
-      dbName: 'fishkapp',
+      dbName: "fishkapp",
     });
-    console.log(colors.green('Database connection successful!'));
+    console.log(colors.green("Database connection successful!"));
   } catch (error) {
-    console.log(colors.red('Error connecting to MongoDB'));
+    console.log(colors.red("Error connecting to MongoDB"));
     process.exit(1);
   }
 });
