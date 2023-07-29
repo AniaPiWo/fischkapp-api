ce250f4928276b3489f5967cf7b923f2491e41f4
AniaPiWo <snowflake@wp.pl>
2023-07-29 12:37:10 +0200

    auth

--- src/server.js
+++ src/server.js
@@ -4,26 +4,18 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import { api } from "./api.js";
-import { config } from "./modules/config.js";
+import { config, corsOptions, authMiddleware } from "./modules/config.js";
 import cors from "cors";
 
 dotenv.config();
-const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN;
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (origin === ALLOWED_ORIGIN) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(cors(corsOptions));
+app.use(authMiddleware);
+
 app.use("/", api);
 
 app.use("/", (req, res) => {
